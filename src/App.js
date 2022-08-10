633de13b9ae00e1cfc5731d584382cfb8c3b0602
Sinan Aksay <sinanaksay@gmail.com>
2022-08-10 16:57:05 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import { ReactComponent as Map } from "./states.svg";
+import USA from "./USA";
+
+export default function App() {
+  return (
+    <div className="App">
+      <USA />
+    </div>
+  );
+}
